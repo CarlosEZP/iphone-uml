6097b353c6a0115dbfe29a8bac97f6a0192b6b1e
Carlos Eduardo Zancanella Pereira <carloszanca03@gmail.com>
2025-02-27 12:35:51 -0300

    Add Phone Caller

---
+++ src/classes/PhoneCaller.java
@@ -0,0 +1,13 @@
+package src.classes;
+
+public class PhoneCaller {
+    public void call(String phone){
+        System.out.println("Calling " + phone);
+    }
+    public void answerCall(){
+        System.out.println("Call was answered");
+    }
+    public void sendVoiceMessage(){
+        System.out.println("You sent a voice message");
+    }
+}
